590de904e858d522606e441a9960122c37f81f6f
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-06 14:56:52 -0700

    The transformer didnt fix this

--- test.ts
+++ test.ts
@@ -1,18 +1,18 @@
 /* eslint-disable no-magic-numbers */
 import takeLast from "./index";
 
-test(() => {
+test("works", () => {
   expect(takeLast(2)(["a", "b", "c"])).toEqual(["b", "c"]);
 });
 
-test(() => {
+test("works", () => {
   expect(takeLast(1)(["a", "b", "c"])).toEqual(["c"]);
 });
 
-test(() => {
+test("works", () => {
   expect(takeLast(2)("abc")).toEqual("bc");
 });
 
-test(() => {
+test("works", () => {
   expect(takeLast(1)("abc")).toEqual("c");
 });
